8ff36425a10ad2e2feeded01810adcd46d2b6205
rukadeveloper <kansaki135@gmail.com>
2026-05-03 17:53:06 +0900

    export 오류 수정

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,4 +1,4 @@
-export { getUserByToken } from './api/user-api';
+export { getUserByTokenApi } from './api/user-api';
 export { userQueries } from './api/user-queries';
 export { UserSchema, type User } from './model/user-schema';
 export { UserBox } from './ui/UserBox';
