a73a398495a6ba5d9d33e0bd8da52cfc2c9d8e79
Jiwon Oh <ohjiwon3040@gmail.com>
2026-05-05 21:07:47 +0900

    feat: 캘린더 지출 백엔드 연동

---
+++ src/entities/master-data/model/master-data-schema.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+const CategorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+const CategoryGroupSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  categories: z.array(CategorySchema),
+});
+
+const EmotionSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+const EmotionGroupSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  emotions: z.array(EmotionSchema),
+});
+
+const SituationTagSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+export const MasterDataSchema = z.object({
+  categoryGroups: z.array(CategoryGroupSchema),
+  emotionGroups: z.array(EmotionGroupSchema),
+  situationTags: z.array(SituationTagSchema),
+});
+
+export type MasterData = z.infer<typeof MasterDataSchema>;
+export type Category = z.infer<typeof CategorySchema>;
+export type Emotion = z.infer<typeof EmotionSchema>;
+export type SituationTag = z.infer<typeof SituationTagSchema>;
